0c342b02692698f5f706b4ffe8a063d1e9d0d667
Devika488 <devika.ed0116@gmail.com>
2026-04-04 10:45:50 +0530

    feat(backend): configure Jest ESM test environment and scripts

---
+++ backend/jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  setupFilesAfterFramework: [
+    '<rootDir>/src/tests/setup.ts'
+  ],
+  moduleNameMapper: {
+    '^(\\.\\.?\\/.*)\\.js$': '$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true
+      }
+    ]
+  },
+  testMatch: [
+    '<rootDir>/src/tests/**/*.test.ts'
+  ]
+};
